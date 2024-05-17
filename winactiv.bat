76b37d864be4f026827a3e732b720f95381ca2b3
AntKill <55269034+AntKill@users.noreply.github.com>
2024-05-17 15:56:46 +0300

    Update winactiv.bat

--- winactiv.bat
+++ winactiv.bat
@@ -1,4 +1,3 @@
 slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX && slmgr /skms kms.digiboy.ir && slmgr /ato
 
-make in folderwith office
-cscript ospp.vbs /sethst:kms.03k.org && cscript ospp.vbs /act
+irm https://massgrave.dev/get | iex
